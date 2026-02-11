d56dfb0eff1b7bf553dcddaef74b9b43a7dc9da9
abinavmugundhan <abinavmugundhan011@gmail.com>
2026-02-12 00:09:27 +0530

    2ndprogram

---
+++ 2nd.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                if d[i]==1:
+                 d[i]+=1
+                else:
+                    del d[i]
+        return list(d.keys())[0]
